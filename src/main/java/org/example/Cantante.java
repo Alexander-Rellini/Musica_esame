9384d080ea8d123aee385c3d2ab3f8d97173acbe
An0n1m0-us <an0n1m0us007.00.1@gmail.com>
2026-01-27 14:44:01 +0100

    commit

---
+++ src/main/java/org/example/Cantante.java
@@ -0,0 +1,37 @@
+package org.example;
+
+public class Cantante {
+    private String nome;
+    private String nazionalità;
+    private String nota_caratteristica;
+
+    public Cantante(String nome, String nota_caratteristica, String nazionalità) {
+        this.nome = nome;
+        this.nota_caratteristica = nota_caratteristica;
+        this.nazionalità = nazionalità;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNazionalità() {
+        return nazionalità;
+    }
+
+    public void setNazionalità(String nazionalità) {
+        this.nazionalità = nazionalità;
+    }
+
+    public String getNota_caratteristica() {
+        return nota_caratteristica;
+    }
+
+    public void setNota_caratteristica(String nota_caratteristica) {
+        this.nota_caratteristica = nota_caratteristica;
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+public class Main{
+
+
+    public static void main(String[] args) {
+        Cantante c = new Cantante("CapoPlaza","NextGen","italiano");
+        Scrittore s = new Scrittore();
+        s.Salvataggio(c);
+
+        try (BufferedReader br = new BufferedReader(new FileReader("music.json"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println("errore nel leggere il file");
+        }
+    }
+}
